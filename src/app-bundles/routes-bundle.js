329e1202a27b80c0ca8128ad36384fed97b81a83
Adam Scarberry <adam.scarberry@usace.army.mil>
2020-12-09 18:25:32 -0500

    added watershed and download

--- src/app-bundles/routes-bundle.js
+++ src/app-bundles/routes-bundle.js
@@ -12,16 +12,20 @@ import Contact from "../app-pages/help/contact";
 import Profile from "../app-pages/profile/profile";
 import ProfileCreate from "../app-pages/profile/create";
 import Basin from "../app-pages/basin/basin-details";
+import WatershedDetail from "../app-pages/basin/basin-details";
 import Basins from "../app-pages/basins/basins";
 import Dashboard from "../app-pages/dashboard/dashboard";
+import Download from "../app-pages/download/download";
 
 export default createRouteBundle(
   {
     "": Home,
     "/": Home,
+    "/ws/:ws_slug": WatershedDetail,
     "/basin/:basin_slug": Basin,
     "/basins": Basins,
     "/dashboard": Dashboard,
+    "/download": Download,
     "/catalog": Catalog,
     "/profile": Profile,
     "/profile/create": ProfileCreate,
