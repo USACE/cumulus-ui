9389319327e95feb35eda05b0831512368ddf5d3
Adam Scarberry <adam.scarberry@usace.army.mil>
2021-03-24 16:01:37 -0400

    button UI added

--- src/app-pages/dashboard/downloads/new-download-button.js
+++ src/app-pages/dashboard/downloads/new-download-button.js
@@ -1,16 +1,17 @@
-import React from "react";
-import { connect } from "redux-bundler-react";
+import React from 'react';
+import { connect } from 'redux-bundler-react';
 
-import NewDownloadModal from "./new-download-modal";
+import NewDownloadModal from './new-download-modal';
 
-const NewDownloadButton = connect("doModalOpen", ({ doModalOpen }) => {
+const NewDownloadButton = connect('doModalOpen', ({ doModalOpen }) => {
   return (
     <button
       onClick={(e) => {
         doModalOpen(NewDownloadModal, {});
       }}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
     >
-      + New
+      + New Download
     </button>
   );
 });
--- src/app-pages/dashboard/my-watersheds/add-my-watershed-button.js
+++ src/app-pages/dashboard/my-watersheds/add-my-watershed-button.js
@@ -1,16 +1,17 @@
-import React from "react";
-import { connect } from "redux-bundler-react";
+import React from 'react';
+import { connect } from 'redux-bundler-react';
 
-import AddMyWatershedModal from "./add-my-watershed-modal";
+import AddMyWatershedModal from './add-my-watershed-modal';
 
-const AddMyWatershedButton = connect("doModalOpen", ({ doModalOpen }) => {
+const AddMyWatershedButton = connect('doModalOpen', ({ doModalOpen }) => {
   return (
     <button
       onClick={(e) => {
         doModalOpen(AddMyWatershedModal, {});
       }}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
     >
-      + New
+      + Add Watershed
     </button>
   );
 });
