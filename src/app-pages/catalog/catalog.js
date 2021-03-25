f5dc7e5cda5957cf68f463e0903a4af89ba28bb6
Adam Scarberry <adam.scarberry@usace.army.mil>
2021-03-25 14:29:38 -0400

    more refactoring

--- src/app-pages/catalog/catalog.js
+++ src/app-pages/catalog/catalog.js
@@ -2,14 +2,7 @@ import React from 'react';
 import Navbar from '../../app-components/navbar';
 import { connect } from 'redux-bundler-react';
 // import moment from 'moment';
-import {
-  format,
-  formatDistance,
-  formatDistanceToNow,
-  formatRelative,
-  subDays,
-  parseISO,
-} from 'date-fns';
+import { formatDistance, formatDistanceToNow, parseISO } from 'date-fns';
 
 function ProductImage(group) {
   switch (group) {
@@ -30,6 +23,7 @@ const ProductRow = (p) => (
       <a href={`/catalog/${p.id}`}>
         <div className="flex items-center">
           <div className="flex-shrink-0 h-10 w-10">
+            {/* <i className="mdi mdi-weather-pouring text-blue-800" /> */}
             <img
               className="h-10 w-10 object-contain"
               src={ProductImage(p.group)}
@@ -46,12 +40,7 @@ const ProductRow = (p) => (
         </div>
       </a>
     </td>
-    <td className="px-6 py-4 whitespace-nowrap">
-      <div className="text-sm text-gray-900">{p.group}</div>
-      <div className="text-sm text-gray-500">
-        {p.parameter}, {p.unit}
-      </div>
-    </td>
+
     <td className="px-6 py-4 whitespace-nowrap">
       <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
         {p.is_realtime && 'Realtime'}
@@ -90,6 +79,25 @@ const ProductRow = (p) => (
   </tr>
 );
 
+const ProductTableHeader = () => (
+  <thead className="bg-gray-50">
+    <tr>
+      {['Product Name', 'Type', 'First Record', 'Last Record', 'Stats'].map(
+        (c) => {
+          return (
+            <th
+              scope="col"
+              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+              {c}
+            </th>
+          );
+        }
+      )}
+    </tr>
+  </thead>
+);
+
 export default connect(
   'selectProductItemsArray',
   ({ productItemsArray: products }) => {
@@ -102,47 +110,8 @@ export default connect(
             <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
               <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                 <table className="min-w-full divide-y divide-gray-200">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Product Name
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Group
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Status
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        First Record
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Last Record
-                      </th>
+                  <ProductTableHeader />
 
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Stats
-                      </th>
-                    </tr>
-                  </thead>
                   <tbody className="bg-white divide-y divide-gray-200">
                     {products.map((p) => ProductRow(p))}
                   </tbody>
