58bd4b8b3795f26de47787fd0afa1f600526ca26
Adam Scarberry <adam.scarberry@usace.army.mil>
2021-05-28 16:01:38 -0400

    fix import case

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'redux-bundler-react';
-import Modal from './app-components/Modal';
+import Modal from './app-components/modal';
 // import Dashboard from './pages/Dashboard';
 
 const App = connect('selectRoute', ({ route: Route }) => {
