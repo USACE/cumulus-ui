4a15071478e729887705c45e45a0d0e9c9b1267e
Adam Scarberry <adam.scarberry@usace.army.mil>
2021-06-02 17:27:56 -0400

    change profile post endpoint

--- src/app-bundles/profile-bundle.js
+++ src/app-bundles/profile-bundle.js
@@ -1,7 +1,7 @@
-import { createSelector } from "redux-bundler";
+import { createSelector } from 'redux-bundler';
 
 const profileBundle = {
-  name: "profile",
+  name: 'profile',
   getReducer: () => {
     const initialState = {
       _shouldFetch: false,
@@ -10,109 +10,115 @@ const profileBundle = {
 
     return (state = initialState, { type, payload }) => {
       switch (type) {
-        case "PROFILE_FETCH_START":
+        case 'PROFILE_FETCH_START':
           return Object.assign({}, state, { _shouldFetch: false });
-        case "PROFILE_FETCH_FINISH":
+        case 'PROFILE_FETCH_FINISH':
           return Object.assign({}, state, { myProfile: payload });
-        case "PROFILE_TOKEN_DELETE_FINISH":
-        case "AUTH_LOGGED_IN":
-        case "AUTH_VERIFY_TOKEN":
-        case "PROFILE_SAVED":
+        case 'PROFILE_TOKEN_DELETE_FINISH':
+        case 'AUTH_LOGGED_IN':
+        case 'AUTH_VERIFY_TOKEN':
+        case 'PROFILE_SAVED':
           return Object.assign({}, state, { _shouldFetch: true });
-        case "PROFILE_TOKEN_DELETE_START":
+        case 'PROFILE_TOKEN_DELETE_START':
           return state;
         default:
           return state;
       }
     };
   },
-  doProfileFetch: () => ({ store, dispatch }) => {
-    dispatch({ type: "PROFILE_FETCH_START" });
+  doProfileFetch:
+    () =>
+    ({ store, dispatch }) => {
+      dispatch({ type: 'PROFILE_FETCH_START' });
 
-    const apiRoot = store.selectApiRoot();
-    const token = store.selectAuthTokenRaw();
+      const apiRoot = store.selectApiRoot();
+      const token = store.selectAuthTokenRaw();
 
-    fetch(`${apiRoot}/my_profile`, {
-      headers: { Authorization: "Bearer " + token },
-    })
-      .then((resp) => {
-        console.log(resp.status);
-        if (resp.status === 404) {
-          console.log("New User; Redirect to /profile/create");
-          store.doUpdateUrl("/profile/create");
-          return {};
-        }
-        return resp.json();
+      fetch(`${apiRoot}/my_profile`, {
+        headers: { Authorization: 'Bearer ' + token },
       })
-      .then((j) => {
-        console.log(j);
-        dispatch({ type: "PROFILE_FETCH_FINISH", payload: j });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  },
-  doProfileSave: (payload) => ({ store, dispatch }) => {
-    const apiRoot = store.selectApiRoot();
-    const authToken = store.selectAuthTokenRaw();
+        .then((resp) => {
+          console.log(resp.status);
+          if (resp.status === 404) {
+            console.log('New User; Redirect to /profile/create');
+            store.doUpdateUrl('/profile/create');
+            return {};
+          }
+          return resp.json();
+        })
+        .then((j) => {
+          console.log(j);
+          dispatch({ type: 'PROFILE_FETCH_FINISH', payload: j });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  doProfileSave:
+    (payload) =>
+    ({ store, dispatch }) => {
+      const apiRoot = store.selectApiRoot();
+      const authToken = store.selectAuthTokenRaw();
 
-    fetch(`${apiRoot}/profiles`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + authToken,
-      },
-      body: JSON.stringify(payload),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          console.log("ERROR!!!!!!!!!!!");
-          console.log(`Request returned a ${response.status}`);
-        }
-        response.json();
+      fetch(`${apiRoot}/my_profile`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + authToken,
+        },
+        body: JSON.stringify(payload),
       })
-      .then((j) => {
-        dispatch({ type: "PROFILE_SAVED" });
-        store.doUpdateUrl("/profile");
-      });
-  },
-  doProfileTokenDelete: (item) => ({ store, dispatch }) => {
-    dispatch({ type: "PROFILE_TOKEN_DELETE_START" });
+        .then((response) => {
+          if (!response.ok) {
+            console.log('ERROR!!!!!!!!!!!');
+            console.log(`Request returned a ${response.status}`);
+          }
+          response.json();
+        })
+        .then((j) => {
+          dispatch({ type: 'PROFILE_SAVED' });
+          store.doUpdateUrl('/profile');
+        });
+    },
+  doProfileTokenDelete:
+    (item) =>
+    ({ store, dispatch }) => {
+      dispatch({ type: 'PROFILE_TOKEN_DELETE_START' });
 
-    const apiRoot = store.selectApiRoot();
-    const authToken = store.selectAuthTokenRaw();
+      const apiRoot = store.selectApiRoot();
+      const authToken = store.selectAuthTokenRaw();
 
-    fetch(`${apiRoot}/my_tokens/${item.token_id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + authToken,
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          console.log("Error!");
-          console.log(`Request returned a ${response.status}`);
-        }
-        response.json();
+      fetch(`${apiRoot}/my_tokens/${item.token_id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + authToken,
+        },
       })
-      .then((j) => {
-        dispatch({ type: "PROFILE_TOKEN_DELETE_FINISH" });
-      });
-  },
+        .then((response) => {
+          if (!response.ok) {
+            console.log('Error!');
+            console.log(`Request returned a ${response.status}`);
+          }
+          response.json();
+        })
+        .then((j) => {
+          dispatch({ type: 'PROFILE_TOKEN_DELETE_FINISH' });
+        });
+    },
   selectProfileRaw: (state) => state.profile,
   selectProfileShouldFetch: (state) => state.profile._shouldFetch,
   selectProfileMyProfile: (state) => state.profile.myProfile,
-  selectProfileTokens: createSelector("selectProfileMyProfile", (profile) =>
+  selectProfileTokens: createSelector('selectProfileMyProfile', (profile) =>
     profile && profile.tokens && profile.tokens.length ? profile.tokens : []
   ),
   reactProfileShouldFetch: createSelector(
-    "selectProfileShouldFetch",
+    'selectProfileShouldFetch',
     (shouldFetch) => {
       if (!shouldFetch) {
         return null;
       }
-      return { actionCreator: "doProfileFetch" };
+      return { actionCreator: 'doProfileFetch' };
     }
   ),
 };
