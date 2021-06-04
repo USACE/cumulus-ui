134ed00233a15a5dbcc5da5eadc3a9eb4a61549e
Adam Scarberry <adam.scarberry@usace.army.mil>
2021-06-04 13:26:30 -0400

    WIP: Edit Tag Modal

--- src/app-pages/modals/edit-tag-modal.js
+++ src/app-pages/modals/edit-tag-modal.js
@@ -1,11 +1,7 @@
 import React, { useState } from 'react';
+import { connect } from 'redux-bundler-react';
 import { HexColorPicker } from 'react-colorful';
 import FormInput from '../forms/forms';
-
-// const ColorPicker = (currentColor) => {
-//   const [color, setColor] = useState(currentColor);
-//   return <HexColorPicker color={color} onChange={setColor} />;
-// };
 
 const handleSubmit = (e) => {
   e.preventDefault();
@@ -24,62 +20,96 @@ const handleSubmit = (e) => {
   // doModalClose();
 };
 
-export default (props) => {
-  const [color, setColor] = useState(props.color);
-  return (
-    <div
-      className='inline-block overflow-visible align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle w-full md:w-3/4 max-w-xl'
-      role='dialog'
-      aria-modal='true'
-      aria-labelledby='modal-headline'
-    >
-      <form className='p-6' onSubmit={handleSubmit}>
-        <fieldset>
-          <legend className='mb-3 text-2xl'>Edit Tag</legend>
+const EditTagModal = connect(
+  'selectModalProps',
+  'doModalClose',
+  ({
+    modalProps: t,
+    doModalClose,
+    // datetimeStart,
+    // datetimeEnd,
+    // watershedSelected,
+    // productsSelected,
+  }) => {
+    // const [payload, setPayload] = useState({
+    //   datetime_start: datetimeStart,
+    //   datetime_end: datetimeEnd,
+    //   watershed_id: watershedSelected || null,
+    //   product_id: productsSelected || [],
+    // });
 
-          <div className='mt-3'>
-            <FormInput
-              label='Name'
-              id='name'
-              maxLength={15}
-              value={props.name}
-            />
-          </div>
+    const [color, setColor] = useState(t.color);
 
-          <div className='mt-3'>
-            <FormInput
-              label='Description'
-              id='description'
-              value={props.description}
-            />
-          </div>
+    return (
+      <div
+        className='inline-block overflow-visible align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle w-full md:w-3/4 max-w-xl'
+        role='dialog'
+        aria-modal='true'
+        aria-labelledby='modal-headline'
+      >
+        <form className='p-6' onSubmit={handleSubmit}>
+          <fieldset>
+            <legend className='mb-3 text-2xl'>Edit Tag</legend>
 
-          <div className='mt-3'>
-            <FormInput label='Color' id='color' maxLength={6} value={color} />
             <div className='mt-3'>
-              <HexColorPicker color={color} onChange={setColor} />
+              <FormInput label='Name' id='name' maxLength={15} value={t.name} />
             </div>
-          </div>
 
-          <div className='flex'>
-            <button
-              onClick={handleSubmit}
-              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-10 rounded mt-3'
-            >
-              Submit
-            </button>
-            <button
-              onClick={(e) => {
-                // doModalClose();
-                return;
-              }}
-              className='ml-3 bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 mt-10 rounded mt-3'
-            >
-              Cancel
-            </button>
-          </div>
-        </fieldset>
-      </form>
-    </div>
-  );
-};
+            <div className='mt-3'>
+              <FormInput
+                label='Description'
+                id='description'
+                value={t.description}
+              />
+            </div>
+
+            <div className='mt-3'>
+              <FormInput
+                label='Color'
+                id='color'
+                maxLength={6}
+                value={color}
+                defaultValue={color}
+              />
+              <div className='flex mt-3'>
+                <HexColorPicker
+                  className='border-black border-2 w-1/2'
+                  color={color}
+                  onChange={(e) => setColor(e.replace('#', ''))}
+                />
+                <div className='flex w-1/2 border-black border-0 items-center place-content-center'>
+                  <span
+                    style={{ backgroundColor: `#${color}` }}
+                    className='text-lg font-light px-2 py-1 rounded-xl'
+                  >
+                    {t.name}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className='flex'>
+              <button
+                onClick={handleSubmit}
+                className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-10 rounded mt-3'
+              >
+                Submit
+              </button>
+              <button
+                onClick={(e) => {
+                  doModalClose();
+                  return;
+                }}
+                className='ml-3 bg-red-300 hover:bg-red-500 text-white font-bold py-2 px-4 mt-10 rounded mt-3'
+              >
+                Cancel
+              </button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
+);
+
+export default EditTagModal;
