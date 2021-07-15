73614b45815d5f14f8ba05043967daae77fc5dad
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2021-07-14 22:03:52 -0400

    develop to stable (#51)
    * adjust download start to remove miliseconds.  Will help with edge cases for forecasts that need to be between start and end dates provided.
    
    * product-tags-modal rewrite to custom table to allow custom logic flexibility for toggle
    
    * hide json payload used for debugging in download modal
    
    * fix apple touch icons reference
    
    * Bugfix/login limbo (#48)
    
    * fix for login limbo
    
    * fix for product availability sort
    
    * fix forceFetchActions
    
    * add svg icons (#50)

--- src/app-pages/profile/token-created-modal.js
+++ src/app-pages/profile/token-created-modal.js
@@ -55,7 +55,7 @@ export default connect('doModalClose', ({ doModalClose, secretTokenInfo }) => {
                 <span className='font-mono'>{secretTokenInfo.token_id}</span>
                 <span>
                   <i
-                    className='mdi mdi-content-copy mdi-18px cursor-pointer'
+                    className='mdi mdi-content-copy mdi-18px cursor-pointer bg-green-500'
                     onClick={(e) => {
                       navigator.clipboard.writeText(secretTokenInfo.token_id);
                     }}
@@ -69,13 +69,35 @@ export default connect('doModalClose', ({ doModalClose, secretTokenInfo }) => {
                 <h5>
                   secret token
                   <i
-                    className={`ml-4 mdi mdi-24px ${
+                    className={`ml-4 cursor-pointer mdi mdi-24px ${
                       secretTokenIsHidden ? 'mdi-eye' : 'mdi-eye-off'
                     }`}
                     onClick={(e) => {
                       setSecretTokenIsHidden(!secretTokenIsHidden);
                     }}
-                  />
+                  >
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      className='h-5 w-5 inline'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <title>show or hide secret token</title>
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth={2}
+                        d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
+                      />
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth={2}
+                        d='M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z'
+                      />
+                    </svg>
+                  </i>
                 </h5>
               </div>
 
@@ -94,7 +116,23 @@ export default connect('doModalClose', ({ doModalClose, secretTokenInfo }) => {
                         secretTokenInfo.secret_token
                       );
                     }}
-                  />
+                  >
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      className='h-6 w-6 inline'
+                      fill='none'
+                      viewBox='0 0 24 24'
+                      stroke='currentColor'
+                    >
+                      <title>copy to clipboard</title>
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth={2}
+                        d='M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3'
+                      />
+                    </svg>
+                  </i>
                 </span>
               </div>
             </div>
