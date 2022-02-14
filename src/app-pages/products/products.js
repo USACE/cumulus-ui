6be38b994553c9c994fec5ffd195218c28bd9fa0
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2022-02-14 13:58:14 -0600

    Products page

--- src/app-pages/products/products.js
+++ src/app-pages/products/products.js
@@ -30,7 +30,7 @@ export default connect(
 
     return (
       <>
-        <div className='m-5 mb-0 inline-flex'>
+        <div className='m-5 mb-5 inline-flex'>
           <ButtonGroup>
             <ButtonGroupButton
               active={filtersActive}
@@ -54,6 +54,8 @@ export default connect(
                 />
               </svg>
             </ButtonGroupButton>
+          </ButtonGroup>
+          <ButtonGroup className='ml-5'>
             <ButtonGroupButton
               active={activeView === 'table'}
               onClick={() => {
@@ -97,9 +99,27 @@ export default connect(
               placeholder='Filter...'
             />
           </span>
+          <ButtonGroup className='ml-5'>
+            <ButtonGroupButton title='Download Selection'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10'
+                />
+              </svg>
+            </ButtonGroupButton>
+          </ButtonGroup>
         </div>
 
-        <div className='flex-grow w-full mx-auto flex'>
+        <div className='w-full mx-auto flex'>
           <DateRangeSlider />
         </div>
 
