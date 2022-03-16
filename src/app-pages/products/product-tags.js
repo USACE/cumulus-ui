ad00ff7fd76e667ae06a2c56e96104e3f0cd2ea1
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-03-16 16:29:27 -0400

    use color stored in db for tag bg

--- src/app-pages/products/product-tags.js
+++ src/app-pages/products/product-tags.js
@@ -1,26 +1,30 @@
 import { connect } from 'redux-bundler-react';
-import { classNames } from '../../utils';
+// import { classNames } from '../../utils';
 
 const Tag = ({ tags, id }) => {
   const tag = tags[id];
-  const cls = classNames(
-    `px-2 inline-flex text-xs leading-5 font-semibold rounded-full`,
-    tag.name === 'Realtime'
-      ? 'bg-teal-300'
-      : tag.name === 'Forecast'
-      ? 'bg-yellow-300'
-      : tag.name === 'Precipitation'
-      ? 'bg-sky-300'
-      : tag.name === 'Temperature'
-      ? 'bg-red-400'
-      : tag.name === 'Archive'
-      ? 'bg-slate-300'
-      : tag.name === 'Snow'
-      ? 'bg-cyan-300'
-      : 'bg-red-800'
-  );
+  // const cls = classNames(
+  //   `px-2 inline-flex text-xs leading-5 font-semibold rounded-full`,
+  //   tag.name === 'Realtime'
+  //     ? 'bg-teal-300'
+  //     : tag.name === 'Forecast'
+  //     ? 'bg-yellow-300'
+  //     : tag.name === 'Precipitation'
+  //     ? 'bg-sky-300'
+  //     : tag.name === 'Temperature'
+  //     ? 'bg-red-400'
+  //     : tag.name === 'Archive'
+  //     ? 'bg-slate-300'
+  //     : tag.name === 'Snow'
+  //     ? 'bg-cyan-300'
+  //     : 'bg-red-800'
+  // );
   return (
-    <span className={cls} title={tag.description}>
+    <span
+      className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full'
+      style={{ backgroundColor: `#${tag.color}` }}
+      title={tag.description}
+    >
       {tag.name}
     </span>
   );
