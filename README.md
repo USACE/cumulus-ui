96c41ce2cc0566466008fffa7a7d0778f29262ac
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2022-03-25 12:00:14 -0500

    Update product availability bundle to do a better job at dealing with date-counts

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### Updating from tailwindui to develop
+
+Did this per `https://www.nickang.com/2017-09-30-replace-git-branch-code/` seemed to work ok.
--- src/app-bundles/product-availability-bundle.js
+++ src/app-bundles/product-availability-bundle.js
@@ -1,8 +1,33 @@
 import createRestBundle from '@usace/create-rest-bundle';
 import { createSelector } from 'redux-bundler';
-import { timeDays, utcWeek } from 'd3';
+import { utcDays, utcWeek } from 'd3';
 
 const apiUrl = process.env.REACT_APP_CUMULUS_API_URL;
+
+function waterYearFromDate(date) {
+  const month = date.getMonth();
+  let year = date.getFullYear();
+  if (month >= 9) year = year + 1;
+  return year;
+}
+
+function scaffoldWaterYear(waterYear) {
+  const dayCache = {};
+  const waterYearStart = new Date(`01-OCT-${waterYear - 1}`);
+  const waterYearEnd = new Date(`01-OCT-${waterYear}`);
+  const days = utcDays(waterYearStart, waterYearEnd);
+  days.forEach((day) => {
+    const x = utcWeek.count(waterYearStart, day);
+    const y = day.getUTCDay();
+    dayCache[`${x}-${y}`] = {
+      x: x,
+      y: y,
+      count: null,
+      date: day,
+    };
+  });
+  return dayCache;
+}
 
 export default createRestBundle({
   name: 'productavailability',
@@ -25,39 +50,24 @@ export default createRestBundle({
         if (!availability || !productYears) return {};
         const availabilityByYear = {};
 
-        productYears.forEach((year, i) => {
-          console.log(year);
-          availabilityByYear[year] = [];
+        availability.date_counts.forEach(({ date, count = 0 }) => {
+          // hack for dealing with UTC times on these datetimes
+          date = date.replace('T00', 'T10');
+          const day = new Date(date);
+          const waterYear = waterYearFromDate(day);
+          const waterYearStart = new Date(`01-OCT-${waterYear - 1}`);
+          const x = utcWeek.count(waterYearStart, day);
+          const y = day.getUTCDay();
 
-          const waterYearStart = new Date(`10-01-${year - 1}`);
-          const waterYearEnd = new Date(`10-01-${year}`);
-          const daysOfWaterYear = timeDays(waterYearStart, waterYearEnd);
+          if (!availabilityByYear[waterYear])
+            availabilityByYear[waterYear] = scaffoldWaterYear(waterYear);
 
-          // probably an easier way to do this...
-          const dayCache = {};
-          daysOfWaterYear.forEach((day) => {
-            const x = utcWeek.count(waterYearStart, day);
-            const y = day.getUTCDay();
-            dayCache[`${x}-${y}`] = {
-              x: x,
-              y: y,
-              count: -1,
-            };
-          });
-
-          // could split these up by year to make this more efficient
-          availability.date_counts.forEach(({ date, count = 0 }) => {
-            const day = new Date(date);
-            const yr = day.getFullYear();
-            if (yr === year) {
-              const x = utcWeek.count(waterYearStart, day);
-              const y = day.getUTCDay();
-              console.log(day, count, x, y);
-              dayCache[`${x}-${y}`].count = count;
-            }
-          });
-
-          availabilityByYear[year] = Object.values(dayCache);
+          availabilityByYear[waterYear][`${x}-${y}`] = {
+            x: x,
+            y: y,
+            count: count,
+            date: day,
+          };
         });
 
         return availabilityByYear;
