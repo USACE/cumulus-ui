c12dd196b54282ba08858bd01e0a1b78e20750c5
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-03-30 20:31:17 -0400

    limit download history to 15days, remove sample download

--- src/app-pages/downloads/downloads-table/downloads-table.js
+++ src/app-pages/downloads/downloads-table/downloads-table.js
@@ -2,28 +2,31 @@ import { connect } from 'redux-bundler-react';
 import DownloadsTableRow from './downloads-table-row';
 
 export default connect(
-  'selectDownloadItems',
+  'selectDownloadItemsPast15d',
   'selectProductItemsObject',
-  function DownloadsTable({ downloadItems, productItemsObject: productsById }) {
-    downloadItems = [
-      ...downloadItems,
-      {
-        id: 'c1de1eb6-5de9-4bd4-b4b9-ac3980ce2ec7',
-        sub: 'f9982695-8ff7-44d9-9b65-b7f0f1b029f2',
-        datetime_start: '2022-03-06T12:00:08Z',
-        datetime_end: '2022-03-09T20:18:08.172Z',
-        watershed_id: 'd79acbe5-7767-4b1f-b07c-03f399265dc9',
-        product_id: ['8b5672d2-2cf1-4ccf-8785-8a9d9302b3a8'],
-        format: null,
-        status: 'SUCCESS',
-        progress: 100,
-        file: 'https://develop-cumulus-api.corps.cloud/cumulus/download/dss7/download_c1de1eb6-5de9-4bd4-b4b9-ac3980ce2ec7.dss',
-        processing_start: '2022-03-09T20:18:18.319931Z',
-        processing_end: '2022-03-09T20:18:27.845405Z',
-        watershed_slug: 'ohio-river-2',
-        watershed_name: 'Ohio River',
-      },
-    ];
+  function DownloadsTable({
+    downloadItemsPast15d: downloadItems,
+    productItemsObject: productsById,
+  }) {
+    // downloadItems = [
+    //   ...downloadItems,
+    //   {
+    //     id: 'c1de1eb6-5de9-4bd4-b4b9-ac3980ce2ec7',
+    //     sub: 'f9982695-8ff7-44d9-9b65-b7f0f1b029f2',
+    //     datetime_start: '2022-03-06T12:00:08Z',
+    //     datetime_end: '2022-03-09T20:18:08.172Z',
+    //     watershed_id: 'd79acbe5-7767-4b1f-b07c-03f399265dc9',
+    //     product_id: ['8b5672d2-2cf1-4ccf-8785-8a9d9302b3a8'],
+    //     format: null,
+    //     status: 'SUCCESS',
+    //     progress: 100,
+    //     file: 'https://develop-cumulus-api.corps.cloud/cumulus/download/dss7/download_c1de1eb6-5de9-4bd4-b4b9-ac3980ce2ec7.dss',
+    //     processing_start: '2022-03-09T20:18:18.319931Z',
+    //     processing_end: '2022-03-09T20:18:27.845405Z',
+    //     watershed_slug: 'ohio-river-2',
+    //     watershed_name: 'Ohio River',
+    //   },
+    // ];
 
     return (
       <div className='flex flex-col w-full ml-5 mr-5'>
