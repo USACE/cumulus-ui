cf2f3877824fa116e2d8e785eac850327cf1f810
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-06-24 15:16:03 -0400

    Feature/home UI update feed additions (#138)
    * new items added to feed
    
    * home ui adjustments for better contrast
    
    * 1.3.6

--- src/app-pages/home/title.js
+++ src/app-pages/home/title.js
@@ -3,7 +3,7 @@ export default function Title() {
     <div className='bg-gray-50'>
       <div className='max-w-7xl mx-auto py-8 px-4 flex flex-row'>
         <div className='max-w-xl'>
-          <h2 className='text-4xl font-extrabold text-black sm:text-5xl sm:tracking-tight lg:text-6xl'>
+          <h2 className='text-4xl font-extrabold text-gray-500 sm:text-5xl sm:tracking-tight lg:text-6xl'>
             USACE Cumulus
           </h2>
         </div>
