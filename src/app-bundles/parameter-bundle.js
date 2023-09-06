235e67793962dd1b5b7f9dd7fcea3e377e9327e5
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2023-09-06 15:02:27 -0400

    trigger fetch based on product saved action

--- src/app-bundles/parameter-bundle.js
+++ src/app-bundles/parameter-bundle.js
@@ -15,7 +15,7 @@ export default createRestBundle({
   deleteTemplate: `${apiURL}/parameters/:item.id`,
   fetchActions: [],
   urlParamSelectors: [],
-  forceFetchActions: ['AUTH_UPDATED'],
+  forceFetchActions: ['AUTH_UPDATED', 'PRODUCT_SAVE_FINISHED'],
   sortBy: 'name',
   sortAsc: true,
   addons: {},
